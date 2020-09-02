865401db8de9771549e88a5e5d6d7eff9a9c84a2
Momoh Philip <momohmayowa14@gmail.com>
2020-09-02 21:59:17 +0100

    portfolio template

---
+++ js/fetch.js
@@ -0,0 +1,32 @@
+
+
+   
+          fetch('sample.json')
+          .then((res) => res.json())
+          .then((data) => {
+
+            for(let i = 0; i < 3; i++){
+            //  output = `
+            //            <div class="col-md-4 col-sm-6 padding-right-zero">
+            //               <div class="portfolio-box design">
+                            
+            //                 ${data[i].id}
+                          
+            //             </div>
+            //             </div>
+            //               `;
+
+                          
+            // document.getElementById('project').innerHTML = output;
+            console.log('sadfde');
+           
+            } 
+
+          })
+    
+
+
+            
+
+        
+          
